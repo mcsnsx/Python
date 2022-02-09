058104d9d578a2e1eff2d56178baa52a369f4fd8
mcsnsx <mcs_nasc@hotmail.com>
2022-02-09 15:04:41 -0300

    Manipulando Arquivos - Banco de Dados

--- Dicionarios/Funcoes.py
+++ Dicionarios/Funcoes.py
@@ -9,3 +9,13 @@ def inserir (dicionario):
     dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
                                                    input("Digite a ultma data de acesso: "),
                                                    input("Qual a ultima estação acessada: ").upper()]
+    salvar(dicionario)
+
+def salvar(dicionario):
+    with open("bd.txt", "a") as arquivo:
+        for chave, valor in dicionario.items():
+            arquivo.write(chave + ":" + str(valor))
+
+def excluir(dicionario):
+    with open("bd.txt", "e") as arquivo:
+        
--- Dicionarios/ManageUsers.py
+++ Dicionarios/ManageUsers.py
@@ -4,6 +4,6 @@ usuarios = {}
 opcao = perguntar()
 while opcao == "I" or opcao == "P" or opcao == "E" or opcao == "L":
     if opcao == "I":
-        inserir(usuarios )
+        inserir(usuarios)
     opcao = perguntar()
 
