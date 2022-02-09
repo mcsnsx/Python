7c709b4fef76aec68275f03a923aeb90ebb0cbd3
mcsnsx <mcs_nasc@hotmail.com>
2022-02-09 14:23:40 -0300

    Manipulação de Arquivos

---
+++ Manipulação_Arquivos/MeuPrimeiroArquivo.py
@@ -0,0 +1,11 @@
+# USO DO A ---> ACRESCENTA AO ARQUIVO EXISTENTE
+#with open("primeiro_arquivo.txt", "a") as arquivo:
+#   arquivo.write("\nHakuna Matata")
+
+# USO DO W ---> CRIA UM NOVO ARQUIVO
+#with open("primeiro_arquivo.txt", "w") as arquivo:
+#   arquivo.write("\nHakuna Matata")
+
+
+with open("primeiro_arquivo.txt", "a") as arquivo:
+    arquivo.write("\nHakuna Matata")
